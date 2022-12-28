5488623440870788cb1059cf54e835ba21ceec27
JamiuKolawole04 <Jakko4real@gmail.com>
2022-12-28 01:51:53 +0100

    chore: modifying post services

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  body: string;
+}
