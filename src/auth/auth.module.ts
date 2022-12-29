cc86eedc985e3104d6c31eef7cebf086c7aee0d0
JamiuKolawole04 <Jakko4real@gmail.com>
2022-12-29 12:59:01 +0100

    chore: adding auth gauard and making changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './strategy/local.strategy';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [PassportModule],
+  imports: [PassportModule, UsersModule],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
 
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
-import { UsersService } from 'src/users/users.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  private userService: UsersService;
-  constructor(private moduleRef: ModuleRef) {
-    this.userService = this.moduleRef.get(UsersService, { strict: false });
-  }
+  constructor(private userService: UsersService) {}
   async validateUser(email: string, password: string) {
     const user = await this.userService.findOneByEmail(email);
 
-    if (user.password !== password) return false;
+    if (!user || user.password !== password) return false;
     return user;
   }
   registerUser(createUser: CreateUserDto) {}
